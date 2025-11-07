09ad50d25ec4ed795cef2cc24682f760eb1deb00
pushpendra yadav <yadavp8120@gmail.com>
2025-11-07 17:52:47 +0530

    changed cookie config

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -51,7 +51,7 @@ const loginUser = async(req, res)=>{
             id: checkUser._id, role : checkUser.role, email: checkUser.email, userName: checkUser.userName
         },'CLIENT_SECRET_KEY', {expiresIn: '60m'})
 
-        res.cookie('token',token, {httpOnly:true, secure: false}).json({
+        res.cookie('token',token, {httpOnly:true, secure: true}).json({
             success:true,
             message: "Logged in successfully",
             user:{
